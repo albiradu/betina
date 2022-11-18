676f37be6b3d4e670528ba9af9befba20a2258a3
albiradu <albiradu@gmail.com>
2022-11-18 11:29:53 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+
+let a=2;
+let b=4;
+
+/*function W(name){
+    console.log("hi",name);
+
+}
+W("albertine");*/
+
+/*function rectangle(w,h){
+    let A=w*h;
+    console.log("the area of rectange",A);
+}
+rectangle(10,5);*/
+
+/*function names(yvette,immaquile){
+    console.log("the names of two peaple is :",yvette,immaquile )
+}
+names("yvette, immaquille");*/
+
+ function checkMyAge(fullName,age){
+      console.log(`hello ${fullName}you have ${age} ages and`);
+     // if (age<20){
+       //  console.log("you are steal young");
+      //}
+      //else{
+        // consol.log("you are an adult");
+      //}
+      let takeAges =(age>20) ? "you are steal young ":"you are an adult"
+ console.log()
+ }
+  checkMyAge("keza belline",17);
+
+  
+//console.log("hello");
+
+
+
+
+
